e1abbd5ddfb1a879828de74218d6e97aaddf9796
RomariosGoIT <romantgt@gmail.com>
2018-07-11 14:13:17 +0300

    update

--- module-11-task-02/index.js
+++ module-11-task-02/index.js
@@ -13,10 +13,34 @@
   https://image.flaticon.com/icons/svg/290/290413.svg
 */
 
-const posts = [
-    { title: "post 1", text: "text 1", isFav: true },
-    { title: "post 2", text: "text 2", isFav: false },
-    { title: "post 3", text: "text 3", isFav: true },
-    { title: "post 4", text: "text 4", isFav: false }
-  ];
-  
+const posts = [{
+        title: "post 1",
+        text: "text 1",
+        isFav: true
+    },
+    {
+        title: "post 2",
+        text: "text 2",
+        isFav: false
+    },
+    {
+        title: "post 3",
+        text: "text 3",
+        isFav: true
+    },
+    {
+        title: "post 4",
+        text: "text 4",
+        isFav: false
+    }
+];
+
+const container = document.querySelector('.container');
+
+const source = document.querySelector('#post-list').innerHTML.trim();
+
+const template = Handlebars.compile(source);
+
+const markup = posts.reduce((acc, post) => acc + template(post), '')
+
+container.insertAdjacentHTML('afterbegin', markup)
